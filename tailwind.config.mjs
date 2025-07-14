3e65a1056101e2bb6098373dc4ae0bf3970e6e80
Oriol <oriolginger2008@gmail.com>
2025-07-14 12:23:43 +0200

    First title

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,20 +2,5 @@ import { defineConfig } from 'tailwindcss';
 
 export default defineConfig({
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
-  theme: {
-    extend: {
-      colors: {
-        bg1: '#0D1117',
-        bg2: '#161B22',
-        textPrimary: '#E6EDF3',
-        textSecondary: '#8B949E',
-        accent1: '#FF6B6B',
-        accent2: '#00D8FF',
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
   plugins: [],
 });
